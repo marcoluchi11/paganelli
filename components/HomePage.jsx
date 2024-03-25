9713e61020cbdd76babec5763cbc10552f96100f
marcoluchi11 <marcoaluchi@hotmail.com>
2024-03-25 11:06:15 -0300

    add homepage component

---
+++ components/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main className="flex flex-col items-center">
+      <h1 className="font-semibold text-4xl my-10">V Paganelli Interiors</h1>
+      <h2 className="font-thin text-3xl mt-20">Coming soon...</h2>
+    </main>
+  );
+}
